5dce2f236c411d3b937546024ab07d4577e08da0
mohdsameerid <mohdsameeredu@gmail.com>
2023-07-14 07:13:33 -0700

    remove element

--- ASS_Array1.java
+++ ASS_Array1.java
@@ -27,3 +27,39 @@ class Solution {
         return answer;
     }
 }
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//  **Q2.** Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+// Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+// - Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+// - Return k.
+
+// **Example :**
+// Input: nums = [3,2,2,3], val = 3
+// Output: 2, nums = [2,2,_*,_*]
+
+// leetcode - https://leetcode.com/problems/remove-element/description/
+
+// Solution
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        // Counter for keeping track of elements other than val
+        int count = 0;
+        // Loop through all the elements of the array
+        for (int i = 0; i < nums.length; i++) {
+            // If the element is not val
+            if (nums[i] != val) {
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
